b7a390c469eef5d00e635f013b3c4c03eae6a4cf
Cristian-SR <santillanrodriguezcristian@gmail.com>
2018-11-21 22:04:21 -0600

    Template Literals with backticks and ${}

--- templateLiterals.js
+++ templateLiterals.js
@@ -19,3 +19,20 @@ to variables. They're also known by the name 'template strings'. Instead
 of concatenating strings using the + opeartor...
 */
 
+
+// ES5
+var generateAddress = function(city, state, country){
+    country = country === undefined ? 'USA' : country;
+    return city + ', ' + state.toUpperCase() + ', ' + country;
+};
+
+var generateAddress = function(city, state, country = 'USA'){
+    return `${city}, ${state.toUpperCase()}, ${country}`;
+};
+
+/*
+... we can embed variables directly into strings using the ${} operator.
+It's important to note...
+that template literals are denoted with backticks ( ` ` ), and not isngle or dougle-quotes.
+*/
+
